054c94e4df2f958daf35227850f9e4e9a96a2a7a
Leon Dai <linchuandai@gmail.com>
2017-11-23 10:45:30 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,134 +1,90 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include<stdbool.h>
+#include <fcntl.h>
+#include <stdbool.h>
 
-/*
-Woooooo it’s a ghost
-Spooky
-fuck your ghost
+#include <ugpio/ugpio.h>
 
-How to curl:
 
-Other things to note:
-You can view content of variable content with:
-echo $content
-You can view content of file content.txt with:
-cat /content.txt
-
-From the onion omega, run:
-
-To get longitude and latitude from IP address, run:
-
-curl freegeoip.net/json/[IPADDRESS] > coordinates.txt
-
-Parse through coordinates.txt to get longitude and latitude
-
-Given any longitude and latitude, we can get the woeid(specifies where the place is) with:
-
-curl query.yahooapis.com/v1/public/yql -d q="select woeid from geo.places where text=\"([LONGITUDE], [LATITUDE])\"" -d format=json > woeid.txt
-
-which stores the woeid in weather.txt
-
-Using the woeid, we can get information on the location’s weather with:
-curl query.yahooapis.com/v1/public/yql -d q="select units,items.forecast from weather.forecast where woeid= [WOEID] and item.forecast.date" -d format=json > weather.txt
-
-This stores the information in weather.txt
-*/
-
-char * substring(char * string, int position, int length){
-    char *pointer;
-    int c;
+struct Button {
+    int pin;
+    int request;
+    int receive;
     
-    pointer = malloc(length+1);
+    int prev_vals[5];
+    int cur_val;
     
-    if (pointer == NULL)
-    {
-        printf("Unable to allocate memory.\n");
-        exit(1);
+    bool pressed;
+};
+
+bool isPressed(int prev_vals[], int cur_val){
+    if(!cur_val)
+        return false;
+    else {
+        for (int i = 0; i < 5; i ++){
+            if(prev_vals[i])
+                return false;
+        }
     }
     
-    for (c = 0 ; c < length ; c++)
-    {
-        *(pointer+c) = *(string+position-1);
-        string++;
-    }
-    
-    *(pointer+c) = '\0';
-    
-    return pointer;
-}
-
-bool getLongandLat(char * buff, char ** latitude, char ** longitude){
-    char * result= strstr(buff, "latitude\":");
-    int LatitudeIndex = result - buff + 11;
-    result= strstr(buff, "longitude\":");
-    int LongitudeIndex = result - buff + 12;
-    int length = LongitudeIndex - LatitudeIndex - 13;
-    
-    *longitude = substring(buff, LongitudeIndex, length);    
-    *latitude = substring(buff, LatitudeIndex, length);
-    
     return true;
-}
+};
 
-bool getWoeid(char * buff, char ** woeid){
-    int woeidIndex = strstr(buff, "woeid\":") - buff + 9;
-    int woeidEndIndex = strstr(buff, "\"}}") - buff + 1;
-    *woeid = substring(buff, woeidIndex, woeidEndIndex - woeidIndex);
-    
-    return true;
-}
-
-bool getTemperature(char * buff, char ** high, char ** low){
-	int highIndex = strstr(buff, "high\":\"") - buff + 8;
-	int lowIndex = strstr(buff, "low\":\"") - buff + 7;
-	int textIndex = strstr(buff, "text\":\"") - buff + 8;
-	
-	*high = substring(buff, highIndex, lowIndex - highIndex - 9);
-	*low = substring(buff, lowIndex, textIndex - lowIndex - 10);
-		
-	return true;	
-}
-
-bool getLineWithString(char * filename, char * string, char * buff){
-	FILE * file;
-	file = fopen(filename, "r");
-	
-	if (file == NULL)
-		return false;
-	
-	while (fscanf(file, "%s", buff) != EOF){
-		if (strstr(buff, string))
-			return true;
-	}
-	return false;
-}
-
-int main(int argc, char** argv)
+int main (int argc, char ** argv, char ** envp)
 {
-    char * IP;
-    FILE * content;
-    char buff[255];
-	
-	char * woeid;
-	char * longitude;
-	char * latitude;
-	char * high;
-	char * low;
-	
+    struct Button button1 = {0};
+    struct Button button2 = {0};
+    struct Button button3 = {0};
+    struct Button button4 = {0};
+    struct Button button5 = {0};
     
-    IP = "129.97.124.0";
-	
-	getLineWithString("content.txt", "high", buff);
-	getTemperature(buff, &high, &low);
-	
-	printf("High: %s , Low: %s ", high, low);
+    struct Button buttons[5] = {0};
     
-    return 0;
+    for (int i = 0; i < 5; i++){
+        buttons[i].pin = i + 1;
+        buttons[i].request = gpio_is_requested(buttons[i].pin);
+        if (buttons[i].request < 0){
+            perror("gpio_is_requested");
+            return EXIT_FAILURE;
+        }
+        if (!buttons[i].request){
+            if (buttons[i].request < 0){
+                perror("gpio_request");
+                return EXIT_FAILURE;
+            }
+        }
+        buttons[i].receive = gpio_direction_input(buttons[i].pin, NULL);
+    }
+    
+    bool run = true;
+    
+    while(run){
+        
+        for (int i = 0; i < 5; i++){
+            buttons[i].cur_val = gpio_get_value(buttons[i].pin);
+            buttons[i].pressed = isPressed(buttons[i].prev_vals, buttons[i].cur_val);
+        }
+         
+         if (button1.pressed){
+         printf("button 1 is pressed\n");
+         }
+         if (button2.pressed){
+         printf("button 2 is pressed\n");
+         }
+         if (button3.pressed){
+         printf("button 3 is pressed\n");
+         }
+         if (button4.pressed){
+         printf("button 4 is pressed\n");
+         }
+         if (button5.pressed){
+         printf("button 5 is pressed\n");
+         }
+     
+        
+    }
+    
 }
+
