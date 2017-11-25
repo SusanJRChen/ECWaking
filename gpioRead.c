86ad0125c8c6ce88c261ac32e74acc3ae62016f6
Matt Belisle <s529chen@edu.uwaterloo.ca>
2017-11-25 11:45:34 -0500

    gpioRead.c edits

--- gpioRead.c
+++ gpioRead.c
@@ -1,127 +1,337 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdbool.h>
+#include <math.h>
 
-#include <ugpio/ugpio.h>
-//#include <onion-i2c.h>
+bool isNum (char in){
+	if (in >= '0' && in <= '9')
+		return true;
+	return false;
+}
 
-struct Button {
-	int pin;
-	int request;
-	int receive;
-	
-	int prev_vals[10];
-	int cur_val;
-
-	bool pressed;	
-};
-
-bool isPressed(int prev_vals[], int cur_val){
-	if(!cur_val)
+bool checkChar(char in){
+	if (in >= 48 && in <= 57)
+		return true;
+	else if(in == 'e' || in == 'E' || in == '.' || in =='+' || in == '-')
+		return true;
+	else
 		return false;
-	else {
-		for (int i = 0; i < 10; i ++){
-			if(prev_vals[i])
-				return false;	
+}
+
+float FahtoCel (float value){
+	return (value - 32) * 1.8;
+}
+
+bool stringToFloat(const char input[], float *value) {
+	enum State { Start, Normal, Terminal };
+	enum InputState { Whole, Decimal, ExponentSign, Exponent };
+	
+	enum State state = Start;
+	enum InputState inState;
+	char in = input[0];
+	int inIndex = 0;
+	bool Done = false;
+	
+	int sign = 1;
+	int exponent = 0;
+	int decimal = 0;
+	int whole = 0;
+	int exponentSign = 1;
+	
+	int decDigits = 0;
+	
+	while (!Done){
+		in = input[inIndex];
+		inIndex++;
+		switch(state){
+		case Start:
+			if (in == '+' || in == '-'){
+				if (!isNum(input[inIndex]))
+					return false;
+				if (in == '-')
+					sign = -1;
+				inState = Whole;
+				state = Normal;
+			}
+			else if(in == '.'){
+				if (!isNum(input[inIndex]))
+					return false;
+				inState = Decimal;
+				state = Normal;
+			}
+			else if (isNum(in)){
+				state = Normal;
+				inState = Whole;
+				whole *= 10;
+				whole += in - 48;				
+			}
+			else
+				return false;
+			break;
+		case Normal:
+			switch(inState){
+			case Whole:
+				if (isNum(in))
+				{
+					whole *= 10;
+					whole += in - 48;
+				}
+				else if (in == '.'){
+					if (!isNum(input[inIndex]))
+						return false;
+					inState = Decimal;
+				}
+				else if (in == 'e' || in == 'E'){
+					if (!isNum(input[inIndex]) && !(input[inIndex] == '+' || input[inIndex] == '-'))
+						return false;
+					inState = ExponentSign;
+				}
+				else if(in == 0)
+					state = Terminal;
+				else if (!checkChar(in) || in == '+' || in == '-')
+					return false;
+				break;
+			case Decimal:
+				if (isNum(in))
+				{
+					decimal *= 10;
+					decimal += in - 48;
+					decDigits++;
+				}
+				else if (in == 'e' || in == 'E'){
+					if (!isNum(input[inIndex]) && !(input[inIndex] == '+' || input[inIndex] == '-'))
+						return false;
+					inState = ExponentSign;
+				}
+				else if(in == 0)
+					state = Terminal;
+				else if (!checkChar(in) || in == '.' || in == '+' || in == '-')
+					return false;
+				break;
+			case ExponentSign:
+				if (isNum(in)){
+					inState = Exponent;
+					exponent *= 10;
+					exponent += in - 48;					
+				}
+				else if (in == '+' || in == '-'){
+					if (!isNum(input[inIndex]))
+						return false;
+					if (in == '-')
+						exponentSign = -1;
+					inState = Exponent;
+				}
+				else if (!checkChar(in) || in == '.')
+					return false;
+				break;
+			case Exponent:
+				if(isNum(in))
+				{
+					exponent *= 10;
+					exponent += in - 48;
+				}
+				else if(in == 0)
+					state = Terminal;
+				else if (!checkChar(in) || in == 'e' || in == 'E' || in == '+' || in == '-')
+					return false;
+				break;
+			}
+			break;
+		case Terminal:
+			Done = true;
+			exponent *= exponentSign;
+			*value = (sign * (whole + decimal* pow(10, -decDigits)) * pow(10, exponent));
+			return true;
+			break;
 		}		
 	}
+}
+
+char * substring(char * string, int position, int length){
+    char *pointer;
+    int c;
+    
+    pointer = malloc(length+1);
+    
+    if (pointer == NULL)
+    {
+        printf("Unable to allocate memory.\n");
+        exit(1);
+    }
+    
+    for (c = 0 ; c < length ; c++)
+    {
+        *(pointer+c) = *(string+position-1);
+        string++;
+    }
+    
+    *(pointer+c) = '\0';
+    
+    return pointer;
+}
+
+bool getIP(char ** ipaddress){
+	FILE *fp = popen("curl ipinfo.io/ip", "r");
+
+	fscanf(fp, "%s", *ipaddress);
+	pclose(fp);
 	
 	return true;
-})
-
-int main (int argc, char ** argv, char ** envp)
-{	
-	struct Button button1 = {0};
-	struct Button button2 = {0};
-	struct Button button3 = {0};
-	struct Button button4 = {0};
-	struct Button button5 = {0};
-
-	button1.pin = 1;
-	button2.pin = 2;
-	
-	button1.request = gpio_is_requested(button1.pin);
-	button2.request = gpio_is_requested(button2.pin);
-	button3.request = gpio_is_requested(button3.pin);
-	button4.request = gpio_is_requested(button4.pin);
-	button5.request = gpio_is_requested(button5.pin);
-	
-	if (button1.request < 0 || button2.request < 0 || button3.request < 0 || button4.request < 0 || button5.request < 0)
-	{
-		perror("gpio_is_requested");
-		return EXIT_FAILURE;
-	}
-	
-	button1.receive = gpio_request(button1.pin, NULL);
-	button2.receive = gpio_request(button2.pin, NULL);
-	button3.receive = gpio_request(button3.pin, NULL);
-	button4.receive = gpio_request(button4.pin, NULL);
-	button5.receive = gpio_request(button5.pin, NULL);
-	
-	if (!(button1.request || button2.request || button3.request || button4.request || button5.request))
-	{
-		if (button1.receive < 0 || button2.receive < 0 || button3.receive < 0 || button4.receive < 0 || button5.receive < 0)
-		{
-			perror("gpio_request");
-			return EXIT_FAILURE;
-		}
-	}
-	
-	button1.receive = gpio_direction_input(button1.pin);
-	button2.receive = gpio_direction_input(button2.pin);
-	button3.receive = gpio_direction_input(button3.pin);
-	button4.receive = gpio_direction_input(button4.pin);
-	button5.receive = gpio_direction_input(button5.pin);
-	
-	bool run = true;
-	
-	while(run){
-		button1.cur_val = gpio_get_value(button1.pin);
-		button2.cur_val = gpio_get_value(button2.pin);
-		button3.cur_val = gpio_get_value(button3.pin);
-		button4.cur_val = gpio_get_value(button4.pin);
-		button5.cur_val = gpio_get_value(button5.pin);		
-		
-		button1.pressed = isPressed(button1.prev_vals, button1.cur_val);
-		button2.pressed = isPressed(button2.prev_vals, button2.cur_val);
-		button3.pressed = isPressed(button3.prev_vals, button3.cur_val);
-		button4.pressed = isPressed(button4.prev_vals, button4.cur_val);
-		button5.pressed = isPressed(button5.prev_vals, button5.cur_val);
-		
-		if (button1.pressed){
-			printf("button 1 is pressed\n");
-		}
-		if (button2.pressed){
-			printf("button 2 is pressed\n");
-		}
-		if (button3.pressed){
-			printf("button 3 is pressed\n");
-		}
-		if (button4.pressed){
-			printf("button 4 is pressed\n");
-		}
-		if (button5.pressed){
-			printf("button 5 is pressed\n");
-		}		
-		
-		
-<<<<<<< HEAD
-		button1.prev_val = button1.cur_val;
-		button2.prev_val = button2.cur_val;
-		button3.prev_val = button3.cur_val;
-		button4.prev_val = button4.cur_val;
-		button5.prev_val = button5.cur_val;	
-		
-		button1.pressed = false;
-		button2.pressed = false;
-		button3.pressed = false;
-		button4.pressed = false;
-		button5.pressed = false;
-=======
->>>>>>> 1c2a59e39cb10a15b28e2f6ed2b7b63afcde4a50
-	}
-	
 }
+
+bool getLongandLat(char * buff, char ** latitude, char ** longitude){
+    char * result= strstr(buff, "latitude\":");
+    int LatitudeIndex = result - buff + 11;
+    result= strstr(buff, "longitude\":");
+    int LongitudeIndex = result - buff + 12;
+    int length = LongitudeIndex - LatitudeIndex - 13;
+    
+    *longitude = substring(buff, LongitudeIndex, length);    
+    *latitude = substring(buff, LatitudeIndex, length);
+    
+    return true;
+}
+
+bool getWoeid(char * buff, char ** woeid){
+    int woeidIndex = strstr(buff, "woeid\":") - buff + 9;
+    int woeidEndIndex = strstr(buff, "\"}}") - buff + 1;
+    *woeid = substring(buff, woeidIndex, woeidEndIndex - woeidIndex);
+    
+    return true;
+}
+
+bool getTemperature(char * buff, char ** high, char ** low){
+	int highIndex = strstr(buff, "high\":\"") - buff + 8;
+	int lowIndex = strstr(buff, "low\":\"") - buff + 7;
+	int textIndex = strstr(buff, "text\":\"") - buff + 8;
+		
+	*high = substring(buff, highIndex, lowIndex - highIndex - 9);
+	*low = substring(buff, lowIndex, textIndex - lowIndex - 10);
+		
+	return true;	
+}
+
+bool getUnits(char * buff, char ** temperatureUnits){
+	int unitIndex = strstr(buff, "temperature\":") - buff + 15;
+	
+	*temperatureUnits = substring(buff, unitIndex, 1);
+	if (*temperatureUnits != "C" && *temperatureUnits != "F")
+		return false;
+	
+	return true;
+}
+
+bool getLineWithString(char * filename, char * string, char * buff){
+	FILE * file;
+	file = fopen(filename, "r");
+	
+	if (file == NULL)
+		return false;
+	
+	while (fscanf(file, "%s", buff) != EOF){
+		if (strstr(buff, string)){
+			return true;			
+		}
+	}
+	return false;
+}
+
+int main(){	
+    char * ipaddress = (char *) malloc(20);
+	char command[256];
+	char buff[256];
+	
+	char * woeid = (char *) malloc(15);
+	char * longitude = (char *) malloc(8);
+	char * latitude = (char *) malloc(8);
+	char * high = (char *) malloc(5);
+	char * low = (char *) malloc(5);
+	char * temperatureUnits = (char *) malloc(1);
+    
+	system("curl ipinfo.io/ip > content.txt");
+	
+	if (getLineWithString("content.txt", ".", buff)){
+		strcpy(ipaddress, buff);
+	}
+	
+	snprintf(command, sizeof command, "%s%s%s", "curl freegeoip.net/json/", ipaddress, " > content.txt");
+	system(command);
+	
+	if (getLineWithString("content.txt", "longitude", buff))
+		getLongandLat(buff, &longitude, &latitude);	
+	
+	snprintf(command, sizeof command, "%s%s%s%s%s", "curl query.yahooapis.com/v1/public/yql -d q=\"select woeid from geo.places where text=\\\"(", longitude, ", " , latitude, ")\\\"\" -d format=json > content.txt");
+	system(command);
+		
+	if (getLineWithString("content.txt", "woeid", buff))
+		getWoeid(buff, &woeid);	
+	
+	
+	snprintf(command, sizeof command, "%s%s%s", "curl query.yahooapis.com/v1/public/yql -d q=\"select * from weather.forecast where woeid=\\\"", woeid, "\\\"\" -d format=json > content.txt");
+	system(command);
+	
+	if (getLineWithString("content.txt", "high", buff))
+		getTemperature(buff, &high, &low);	
+	
+	if (getLineWithString("content.txt", "temperature", buff))
+		getUnits(buff, &temperatureUnits);
+	
+	float highfloat = 0;
+	float lowfloat = 0;
+	
+	stringToFloat(high, &highfloat);
+	stringToFloat(low, &lowfloat);	
+	
+	if (temperatureUnits == "F"){
+		highfloat = FahtoCel(highfloat);
+		lowfloat = FahtoCel(lowfloat);
+	}
+	
+	float averageTemperature = (highfloat + lowfloat)/2;
+		
+	printf("Public IP: %s\n", ipaddress);
+	printf("Coordinates: (%s, %s)\n", latitude, longitude);
+	printf("woeid: %s\n", woeid);
+	printf("Units were given in: %s\n", temperatureUnits);
+	printf("(in celsius) High: %f , Low: %f\n", highfloat, lowfloat);
+	printf("Average Temperature(C): %f\n", averageTemperature);
+	
+	
+	return 0;
+}
+
+/*
+Woooooo it’s a ghost
+Spooky
+fuck your ghost
+
+How to curl:
+
+Other things to note:
+You can view content of variable content with:
+echo $content
+You can view content of file content.txt with:
+cat /content.txt
+
+From the onion omega, run:
+
+To get longitude and latitude from IP address, run:
+
+curl freegeoip.net/json/[IPADDRESS] > content.txt
+
+Parse through coordinates.txt to get longitude and latitude
+
+Given any longitude and latitude, we can get the woeid(specifies where the place is) with:
+
+curl query.yahooapis.com/v1/public/yql -d q="select woeid from geo.places where text=\"([LONGITUDE],[LATITUDE])\"" -d format=json > content.txt
+
+which stores the woeid in weather.txt
+
+Using the woeid, we can get information on the location’s weather with:
+
+curl query.yahooapis.com/v1/public/yql -d q="select * from weather.forecast where woeid=[WOEID]" -d format=json > weather.txt
+
+This stores the information in weather.txt
+*/
